916dfc6208ffacd4078d35f3c45c9cf0984217e5
zick@caffeine-soft.org <zick@caffeine-soft.org@26d25ef3-d744-0410-ba35-c9b3002ec803>
2008-03-31 18:03:14 +0000

    register account fixes

--- icqlib/src/ru/caffeineim/protocols/icq/packet/received/generic/UINRegistrationFailed__17_1.java
+++ icqlib/src/ru/caffeineim/protocols/icq/packet/received/generic/UINRegistrationFailed__17_1.java
@@ -1,3 +1,18 @@
+/**
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package ru.caffeineim.protocols.icq.packet.received.generic;
 
 import ru.caffeineim.protocols.icq.core.OscarConnection;
@@ -5,19 +20,20 @@ import ru.caffeineim.protocols.icq.integration.events.UINRegistrationFailedEvent
 import ru.caffeineim.protocols.icq.integration.listeners.MetaInfoListener;
 import ru.caffeineim.protocols.icq.packet.received.ReceivedPacket;
 
-public class UINRegistrationFailed__17_1 extends ReceivedPacket
-{
-    public UINRegistrationFailed__17_1(byte array[])
-    {
+/**
+ * <p>Created by
+ *   @author Egor Baranov 
+ */
+public class UINRegistrationFailed__17_1 extends ReceivedPacket {
+    public UINRegistrationFailed__17_1(byte array[]) {
         super(array, true);
     }
-    
-    public void notifyEvent(OscarConnection connection) 
-    {
+
+    public void notifyEvent(OscarConnection connection) {
         UINRegistrationFailedEvent e = new UINRegistrationFailedEvent(this);
-        for (int i = 0; i < connection.getMetaInfoListeners().size(); i++)
-        {
-            MetaInfoListener l = (MetaInfoListener)connection.getMessagingListeners().get(i);
+        for (int i = 0; i < connection.getMetaInfoListeners().size(); i++) {
+            MetaInfoListener l = (MetaInfoListener) connection
+                    .getMessagingListeners().get(i);
             l.registerNewUINFailed(e);
         }
     }
--- icqlib/src/ru/caffeineim/protocols/icq/packet/received/generic/UINRegistrationSuccess__17_5.java
+++ icqlib/src/ru/caffeineim/protocols/icq/packet/received/generic/UINRegistrationSuccess__17_5.java
@@ -1,3 +1,18 @@
+/**
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package ru.caffeineim.protocols.icq.packet.received.generic;
 
 import ru.caffeineim.protocols.icq.core.OscarConnection;
@@ -5,32 +20,29 @@ import ru.caffeineim.protocols.icq.integration.events.UINRegistrationSuccessEven
 import ru.caffeineim.protocols.icq.integration.listeners.MetaInfoListener;
 import ru.caffeineim.protocols.icq.packet.received.ReceivedPacket;
 
-public class UINRegistrationSuccess__17_5 extends ReceivedPacket
-{
+/**
+ * <p>Created by
+ *   @author Egor Baranov 
+ */
+public class UINRegistrationSuccess__17_5 extends ReceivedPacket {
     protected String uin;
-    
-    public UINRegistrationSuccess__17_5(byte array[])
-    {
+
+    public UINRegistrationSuccess__17_5(byte array[]) {
         super(array, true);
+        int num = this.getSnac().getByteArray()[11] << 8 + this.getSnac()
+                .getByteArray()[12];
+        uin = String.valueOf(num);
     }
-    
-    public String getNewUIN()
-    {
-        if (uin == null)
-        {
-            int num = this.getSnac().getByteArray()[11] << 8 + this.getSnac().getByteArray()[12];
-            uin = String.valueOf(num);
-        }
-        
+
+    public String getNewUIN() {
         return uin;
     }
-    
-    public void notifyEvent(OscarConnection connection) 
-    {
+
+    public void notifyEvent(OscarConnection connection) {
         UINRegistrationSuccessEvent e = new UINRegistrationSuccessEvent(this);
-        for (int i = 0; i < connection.getMetaInfoListeners().size(); i++)
-        {
-            MetaInfoListener l = (MetaInfoListener)connection.getMessagingListeners().get(i);
+        for (int i = 0; i < connection.getMetaInfoListeners().size(); i++) {
+            MetaInfoListener l = (MetaInfoListener) connection
+                    .getMessagingListeners().get(i);
             l.registerNewUINSuccess(e);
         }
     }
