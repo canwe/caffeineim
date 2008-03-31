034ae34dd94ee8f04227ca538675896d1575de7f
zick@caffeine-soft.org <zick@caffeine-soft.org@26d25ef3-d744-0410-ba35-c9b3002ec803>
2008-03-31 18:03:14 +0000

    register account fixes

--- trunk/icqlib/src/ru/caffeineim/protocols/icq/integration/events/UINRegistrationFailedEvent.java
+++ trunk/icqlib/src/ru/caffeineim/protocols/icq/integration/events/UINRegistrationFailedEvent.java
@@ -1,15 +1,32 @@
+/**
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package ru.caffeineim.protocols.icq.integration.events;
 
 import java.util.EventObject;
 
 import ru.caffeineim.protocols.icq.packet.received.generic.UINRegistrationFailed__17_1;
 
-public class UINRegistrationFailedEvent  extends EventObject
-{
+/**
+ * <p>Created by
+ *   @author Egor Baranov 
+ */
+public class UINRegistrationFailedEvent extends EventObject {
     private static final long serialVersionUID = 1;
-    
-    public UINRegistrationFailedEvent(UINRegistrationFailed__17_1 source)
-    {
+
+    public UINRegistrationFailedEvent(UINRegistrationFailed__17_1 source) {
         super(source);
     }
 }
