9fccfe2b1729ca8445ab762eb021ddc81ff87bdf
andrew.vrn <andrew.vrn@26d25ef3-d744-0410-ba35-c9b3002ec803>
2009-01-15 10:45:47 +0000

    Добавлены наброски для окна сообщений

---
+++ caffeineim/src/ru/caffeineim/BuddyInfo.java
@@ -0,0 +1,17 @@
+package ru.caffeineim;
+
+/**
+ * @date 15.01.2009
+ * @author Andrew R/B
+ */
+public class BuddyInfo {
+
+    public static String getMyNick() {
+        return "Me";
+    }
+
+    public static String getNickByUin(String uin){
+        return uin;
+    }
+
+}
